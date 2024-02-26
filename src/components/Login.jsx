f362326f31003fd54e6b1a646ea9d526a430243e
Sridhar <kndlsridhar@gmail.com>
2024-02-26 17:27:55 +0530

    login design

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,33 +1,45 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React,{ useState } from "react";
 
 
 const Login = () => {
-    const [loginData, setLoginData] = useState({
-      email: '',
-      password: ''
-    });
-  
-    const handleChange = e => {
-      setLoginData({ ...loginData, [e.target.name]: e.target.value });
-    };
-  
-    const handleSubmit = async e => {
-      e.preventDefault();
-      try {
-        const response = await axios.post('/api/login', loginData);
-        console.log(response.data); // handle successful login response
-      } catch (error) {
-        console.error('Login error:', error);
-      }
-    };
-  
+  const [data,setData] = useState({
+    username:'',
+    password:''
+  })
+
+  //distructuring
+  const {username,password} = data;
+
+  //onchange
+
+  const changeHandler = e =>{
+    setData({...data,[e.target.name]:[e.target.value]})
+  }
+
+  // onsubmit
+  const submitHandler = e =>{
+    e.preventDefault()
+    console.log(data)
+  }
     return (
-      <form onSubmit={handleSubmit}>
-        <input type="email" name="email" placeholder="Email" onChange={handleChange} />
-        <input type="password" name="password" placeholder="Password" onChange={handleChange} />
-        <button type="submit">Login</button>
-      </form>
+      
+      <div className="container">
+        <div className="row justify-content-center align-items-center">
+          <div className="col-4 col-lg-4 col-xl-4">
+            <div className="card">
+              <div className="card-body">
+                <h4 className="mb-3">Login</h4>
+                <form onSubmit={submitHandler}>
+                  <input type="text" name="username" className="form-control mb-3" placeholder="Email" value={username} onChange={changeHandler}/>
+                  <input type="password" name="password" className="form-control mb-3" placeholder="Password" value={password} onChange={changeHandler} />
+                  <button type="submit" className="btn btn-dark w-100 btn-block" name="submit">Login</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
     );
   };
   
